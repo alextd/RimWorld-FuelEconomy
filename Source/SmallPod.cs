d2d432cac107efd5f72be625bf26b80df1d921d4
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-09-14 10:03:08 -0700

    Translatable

--- Source/SmallPod.cs
+++ Source/SmallPod.cs
@@ -33,7 +33,7 @@ namespace Fuel_Economy
 			}
 			else if (transferables.Any(t => t.CountToTransfer > 0 && t.AnyThing.GetStatValue(StatDefOf.Mass) > maxMass))
 			{
-				Messages.Message($"An item is too large to fit in a small transport pod ({maxMass}kg max)", MessageTypeDefOf.RejectInput);
+				Messages.Message("TD.SmallTransportPodTooLarge".Translate(new object[] { maxMass }), MessageTypeDefOf.RejectInput);
 				__result = false;
 			}
 		}
