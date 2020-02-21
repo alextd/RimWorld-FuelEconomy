d2da3ff6d82c85fd88ebebeffc22703652a83007
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2020-02-21 11:13:35 -0800

    Clarifying names and log lines

--- Source/LaunchableFuel.cs
+++ Source/LaunchableFuel.cs
@@ -23,33 +23,33 @@ namespace Fuel_Economy
 		public static float smallPodEfficiency = 5;
 		public static float FuelNeededToLaunchAtDist(float dist, CompLaunchable launchable)
 		{
-			float fuelForPod = MassFactor(launchable) * dist;
+			float fuelForPod = FuelPerTile(launchable) * dist;
 			if (launchable.parent.def == SmallPodDefOf.TransportPodSmall)
 				fuelForPod /= smallPodEfficiency;
-			Log.Message($"Needs {fuelForPod}");
+			Log.Message($"At {dist}, {launchable.parent} needs {fuelForPod} fuel with {FuelPerTile(launchable)} Fuel per file");
 			return fuelForPod;
 		}
 
 		static int vanillaMax = 66; //150 / 2.25
 		public static int MaxLaunchDistanceAtFuelLevel(float fuelLevel, CompLaunchable launchable)
 		{
-			float distance = fuelLevel / MassFactor(launchable);
+			float distance = fuelLevel / FuelPerTile(launchable);
 			if (launchable.parent.def == SmallPodDefOf.TransportPodSmall)
 				distance *= smallPodEfficiency;
 			if (!Settings.Get().pastVanillaMaxRange && distance > vanillaMax)
 				distance = vanillaMax;
-			Log.Message($"Can do {distance}");
+			Log.Message($"Can do {distance} for {fuelLevel} fuel with {FuelPerTile(launchable)} Fuel per file");
 			return Mathf.FloorToInt(distance);
 		}
 
 		// emptyPercent of 50% means a pod weighs 150kg, so a full pod weighs 300kg and an empty pod costs 50% fuel, and can go 2x as far
 		// 0% means it's weightless, can send empty pod with 0% fuel
 		// 100% means essentially infinite weight, all fuel is used to launch pod and the content doesn't matter, and that's vanilla so why do that
-		const float FuelPerTile = 2.25f;
-		public static float MassFactor(CompLaunchable launchable)
+		const float vanillaFuelPerTile = 2.25f;
+		public static float FuelPerTile(CompLaunchable launchable)
 		{
 			float emptyPercent = Settings.Get().emptyPercent;
-			return ((FuelPerTile * emptyPercent) + (FuelPerTile - FuelPerTile * emptyPercent) * PercentFull(launchable));
+			return vanillaFuelPerTile * (emptyPercent + (1 - emptyPercent) * PercentFull(launchable));
 		}
 
 		public static float PercentFull(CompLaunchable launchable)
@@ -107,13 +107,13 @@ namespace Fuel_Economy
 		//private void TryLaunch(GlobalTargetInfo target, PawnsArriveMode arriveMode, bool attackOnArrival)
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
 		{
-			MethodInfo MaxLaunchDistanceAtFuelLevel = AccessTools.Method(typeof(CompLaunchable), "MaxLaunchDistanceAtFuelLevel");
-			MethodInfo MaxLaunchDistanceAtFuelLevelPatch = AccessTools.Method(typeof(LaunchableMethods), "MaxLaunchDistanceAtFuelLevel",
+			MethodInfo MaxLaunchDistanceAtFuelLevelInfo = AccessTools.Method(typeof(CompLaunchable), nameof(CompLaunchable.MaxLaunchDistanceAtFuelLevel));
+			MethodInfo MaxLaunchDistanceAtFuelLevelPatch = AccessTools.Method(typeof(LaunchableMethods), nameof(LaunchableMethods.MaxLaunchDistanceAtFuelLevel),
 				new Type[] { typeof(float), typeof(CompLaunchable) });
 
 			foreach (CodeInstruction i in codeInstructions)
 			{
-				if (i.opcode == OpCodes.Call && i.operand.Equals(MaxLaunchDistanceAtFuelLevel))
+				if (i.opcode == OpCodes.Call && i.operand.Equals(MaxLaunchDistanceAtFuelLevelInfo))
 				{
 					i.operand = MaxLaunchDistanceAtFuelLevelPatch;
 					yield return new CodeInstruction(OpCodes.Ldarg_0); //this
