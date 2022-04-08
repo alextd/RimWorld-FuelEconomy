dd766f8b98ed0d04ebf163efec4356d53e0d700c
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-04-08 14:04:20 -0700

    better reflection via harmony

--- Source/SmallPod.cs
+++ Source/SmallPod.cs
@@ -12,9 +12,8 @@ namespace Fuel_Economy
 	[HarmonyPatch(typeof(Dialog_LoadTransporters), "CheckForErrors")]
 	class SmallPod
 	{
-		public static FieldInfo transferablesInfo = AccessTools.Field(typeof(Dialog_LoadTransporters), "transferables");
 		//private bool CheckForErrors(List<Pawn> pawns)
-		public static void Postfix(ref bool __result, Dialog_LoadTransporters __instance, List<CompTransporter> ___transporters)
+		public static void Postfix(ref bool __result, Dialog_LoadTransporters __instance, List<CompTransporter> ___transporters, List<TransferableOneWay> ___transferables)
 		{
 			if (!__result) return;
 
@@ -24,14 +23,13 @@ namespace Fuel_Economy
 			float maxMass = ___transporters[0].Props.massCapacity;
 			Log.Message($"maxMass is {maxMass}");
 
-			List <TransferableOneWay> transferables = (List<TransferableOneWay>)transferablesInfo.GetValue(__instance);
-			Log.Message($"transferables are {transferables}");
-			if (transferables.Any(t => t.CountToTransfer > 0 && t.AnyThing is Pawn))
+			Log.Message($"transferables are {___transferables}");
+			if (___transferables.Any(t => t.CountToTransfer > 0 && t.AnyThing is Pawn))
 			{
 				Messages.Message("TD.SmallTransportPodItemsOnly".Translate(), MessageTypeDefOf.RejectInput);
 				__result = false;
 			}
-			else if (transferables.Any(t => t.CountToTransfer > 0 && t.AnyThing.GetStatValue(StatDefOf.Mass) > maxMass))
+			else if (___transferables.Any(t => t.CountToTransfer > 0 && t.AnyThing.GetStatValue(StatDefOf.Mass) > maxMass))
 			{
 				Messages.Message("TD.SmallTransportPodTooLarge".Translate(maxMass), MessageTypeDefOf.RejectInput);
 				__result = false;
